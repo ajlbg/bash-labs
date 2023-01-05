1309b0a531b6bcbf6090a0e22a4a49257e15a366
Ashley <ashley.jefford@lloydsbanking.com>
2023-01-05 12:30:59 +0000

    Adds problem description

--- Challenges/Replace Header Challenge Answer/ReplaceHeader.sh
+++ Challenges/Replace Header Challenge Answer/ReplaceHeader.sh
@@ -1,2 +1,9 @@
+# A company has noticed some old data files that do not follow their current branding.
+# Each data file should contain the brand name, HQ, and founding date as a header, followed by the content of the file
+
+# Write a script to insert the content of Header.txt on top of Data.txt to update it with the correct branding. 
+
+# Extension: Update your script so it can add the header data to all files in a directory.
+
 cat Header.txt Data.txt > Data.NEW
 mv -f Data.new Data.txt
